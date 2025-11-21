591b9852e2870f5adb58ca9ef2656026efbd4361
Sadhana Jayakumar <sadhanajayakumar@Sadhanas-MacBook-Pro-2.local>
2025-11-21 13:37:46 +0000

    Skeleton code for the camp model. Contains information like all the fields and some of the basic functionalities for the camp

--- models/camp.py
+++ models/camp.py
@@ -1,3 +1,90 @@
+import uuid
+from datetime import datetime, timedelta
+
 class Camp:
-    def __init__(self):
-        pass
+    # 1. Camp will be defined by the logistics coordinator
+    # 2. Class contains information like 
+    #     1. Name
+    #     2. Location
+    #     3. Dates of availability 
+    #     4. Type of the camp (day, overnight etc)
+    #     5. Food stock information 
+    #     6. Leader information
+    #     7. Campers data information
+    
+
+    def __init__(
+        self, 
+        name: str, 
+        location: str, 
+        camp_type: str, 
+        start_date: str, 
+        end_date: str, 
+        intial_food_stock_per_day: int
+
+    ): 
+        self.camp_id = str(uuid.uuid4()); #generate the uniqueId for a camp
+        self.name = name
+        self.location = location
+        self.camp_type = camp_type
+        self.start_date = start_date
+        self.end_date = end_date
+        self.intial_food_stock_per_day = intial_food_stock_per_day
+        self.food_stock_information = {}
+        self.camp_leader = None #needs to be set, initially no leader by default
+        self.campers = [] #will be an array of "campers class"
+        self.intialize_food_information()
+    
+
+
+    def intialize_food_information(self):
+        #camps mayb running across multiple days 
+        #we need to go through each of those days and initialize the food amount
+        start = datetime.strptime(self.start_date, "%Y-%m-%d") #convert date time to a propper format
+        end = datetime.strptime(self.end_date, "%Y-%m-%d") 
+
+        current = start
+        while(current <= end):
+            date_str = current.strftime("%Y-%m-%d")
+            self.food_stock_information[date_str] = self.intial_food_stock_per_day
+            current += timedelta(days = 1) #increment by one day
+
+    
+    def topup_food(self, amount):
+        for date in self.food_stock_information:
+            self.food_stock_information[date] += amount
+    
+
+    #Caluclate the total food requirements for the camp
+    def total_food_required(self, food_per_camp_per_day):
+        total_campers = len(self.campers)
+        start = datetime.strptime(self.start_date, "%Y-%m-%d")
+        end = datetime.strptime(self.end_date, "%Y-%m-%d")
+        days = (end - start).days + 1
+
+        return total_campers * food_per_camp_per_day * days
+    
+
+    def is_food_shortage(self):
+        total_campers = len(self.campers)
+        for date, stock in self.food_stock_information.items():
+            if stock < total_campers:
+                return True
+            
+        return False
+    
+
+    def add_camper(self, camper):
+        self.campers.append(camper)
+    
+    def remove_camper(self, camper_id):
+        for camper in self.campers:
+            if camper.camper_id == camper_id:
+                self.campers.remove(camper)
+                break
+    
+    def assign_leader(self, username):
+        self.camp_leader = username
+    
+
+
