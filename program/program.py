f7e9982075911660eda31ea54cee52094e784f63
Huangxin Dong <donghx2002@outlook.com>
2025-12-10 19:08:23 +0000

    Add AdminController handling in main program loop

--- program/program.py
+++ program/program.py
@@ -2,7 +2,17 @@
 # Responsible for main program loop
 import interface
 
+import interface
+from models.users.admin import Admin
+from controllers.controller_admin import AdminController
+
 def run_program(user):
+    # If the user is an Admin, use the AdminController (MVC pattern)
+    if isinstance(user, Admin):
+        controller = AdminController(user)
+        controller.run()
+        return
+
     run_program = True
     while run_program:
         run_program = interface.main_ui.process_command(user)
