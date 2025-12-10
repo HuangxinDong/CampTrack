658f37028a9276fd7ece046cfc10172a6cad8d21
maayancarno <maayan.carno@outlook.com>
2025-12-10 20:15:09 +0000

    delegte rdeundant code

--- models/users/admin.py
+++ models/users/admin.py
@@ -1,4 +1,3 @@
-
 from models.users.class_map import register
 from models.users.users import User
 
@@ -10,21 +9,5 @@ class Admin(User):
     """
     def __init__(self, username, password, role="Admin", enabled=True):
         super().__init__(username, password, role, enabled)
-        self.commands += [
-            { 'name': 'Create User', 'command': self.handle_create_user },
-            { 'name': 'Delete User', 'command': self.handle_delete_user },
-        ]
-
-    def handle_create_user(self):
-        username = input("Enter username: ")
-        password = input("Enter password: ")
-        role = input("Enter role (Leader/Coordinator): ")
-        
-        success, message = self.user_manager.create_user(username, password, role)
-        print(message)
-
-    def handle_delete_user(self):
-        username = input("Enter username to delete: ")
-        success, message = self.user_manager.delete_user(username)
-        print(message)
+       
 
--- models/users/leader.py
+++ models/users/leader.py
@@ -1,22 +1,10 @@
-from persistence.dao.camp_manager import CampManager
 from models.users.class_map import register
 from models.users.users import User
-from cli.prompts import get_positive_int
-from datetime import datetime
-
-
 @register("Leader")
 class Leader(User):
-    def __init__(self, username, password, role, enabled, daily_payment_rate):
+    def __init__(self, username, password, role="Leader", enabled=True, daily_payment_rate=0):
         super().__init__(username, password, role, enabled)
         self.daily_payment_rate = daily_payment_rate
-        self.parent_commands = [
-            {'name': 'Select Camps to Supervise', 'command': self.select_camps_to_supervise},
-            {'name': 'Edit Camp', 'command': self.edit_camp},
-        ]
-        self.commands = self.parent_commands
-        self.camp_manager = CampManager()
-    
 
     def to_dict(self):
         data = super().to_dict()
